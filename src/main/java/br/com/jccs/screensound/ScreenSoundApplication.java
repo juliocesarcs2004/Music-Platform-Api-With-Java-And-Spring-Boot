108848e8711284b315739d0bcff5d3c5b29634fa
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-03-09 12:00:12 -0300

    Created database and connect project with database

---
+++ src/main/java/br/com/jccs/screensound/ScreenSoundApplication.java
@@ -0,0 +1,20 @@
+package br.com.jccs.screensound;
+
+import br.com.jccs.screensound.Principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenSoundApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+}
